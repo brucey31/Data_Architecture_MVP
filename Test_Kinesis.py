3721fb40d1a89f5a684708d9dc34d9f2c6fd9ddd
Bruce Pannaman <Bruce@ip-192-168-0-7.eu-west-1.compute.internal>
2016-05-05 17:42:36 +0100

    first commit

---
+++ Test_Kinesis.py
@@ -0,0 +1,49 @@
+from boto import kinesis
+import time
+import json
+import csv
+import configparser
+
+
+config = configparser.ConfigParser()
+ini = config.read('conf2.ini')
+
+AWS_ACCESS_KEY_ID = config.get('AWS Credentials', 'key')
+AWS_SECRET_ACCESS_KEY = config.get('AWS Credentials', 'secret')
+
+kinesis = kinesis.connect_to_region("eu-west-1", aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+
+print kinesis.list_streams()
+print kinesis.describe_stream("rawdata")
+
+initial_uid = 32767376
+
+with open("Kinesis_Test_Data.csv", 'rb') as source_file:
+    contents = csv.reader(source_file, delimiter=',', quotechar='|')
+
+    for event in contents:
+        data = dict()
+
+        initial_uid = initial_uid + 1
+        data['uid'] = initial_uid
+        data['event'] = event[0]
+        data['timestamp'] = str(time.strftime("%Y-%m-%d %H:%m:%S"))
+        data['unit'] = event[1]
+        data['package'] = event[2]
+        data['price'] = event[3]
+        data['platform'] = event[4]
+
+        json_data = json.dumps(data, ensure_ascii=False)
+
+        print json_data
+
+        kinesis.put_record("rawdata", json_data, "partitionkey")
+
+
+shard_id = 'shardId-000000000000'
+shard_it = kinesis.get_shard_iterator("rawdata", shard_id, "LATEST")["ShardIterator"]
+while 1 == 1:
+    out = kinesis.get_records(shard_it, limit=2)
+    shard_it = out["NextShardIterator"]
+    print out
+    time.sleep(0.2)
