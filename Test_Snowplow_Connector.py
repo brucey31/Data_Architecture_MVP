10583c877a609dc083c03aee1784f33e5cd0a176
Bruce Pannaman <Bruce@ip-192-168-0-20.eu-west-1.compute.internal>
2016-06-11 07:41:47 +0100

    getting 200 requests sent to the stream collector

--- Test_Snowplow_Connector.py
+++ Test_Snowplow_Connector.py
@@ -1,21 +1,29 @@
 from snowplow_tracker import Subject, Tracker, Emitter
 from snowplow_tracker import SelfDescribingJson
+import csv
 
-e = Emitter("d3rkrsqld9gmqf.cloudfront.net")
-
-
-
+e = Emitter("10.0.52.22", port=8080)
 t = Tracker(e)
 s = Subject()
-s.set_user_id( "{{USER ID}}" )
 
 
-t.track_unstruct_event(SelfDescribingJson(
-  "com.example_company/save-game/jsonschema/1-0-2",
-  {
-    "save_id": "4321",
-    "level": 23,
-    "difficultyLevel": "HARD",
-    "dl_content": True
-  }
-)
+while 1 == 1 :
+    with open('Kinesis_Test_Data.csv', 'rb') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        for row in spamreader:
+            event_name = row[0]
+            unit_id = row[1]
+            package = row[2]
+            price = row[3]
+            platform = row[4]
+            uid = row[5]
+
+            s.set_platform("pc").set_user_id(uid).set_lang("enc")
+
+            t.track_unstruct_event(SelfDescribingJson(
+                "com.example_company/save-game/jsonschema/1-0-2",
+                {"event": event_name,
+                "package": package,
+                "unit_id": unit_id,
+                "price": price
+                }))
