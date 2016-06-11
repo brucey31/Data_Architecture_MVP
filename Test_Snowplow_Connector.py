959665f1d51d06b9582519653bb7fc68c1ba0a89
Bruce Pannaman <Bruce@ip-192-168-0-20.eu-west-1.compute.internal>
2016-06-11 07:44:59 +0100

    now includes time

--- Test_Snowplow_Connector.py
+++ Test_Snowplow_Connector.py
@@ -1,6 +1,7 @@
 from snowplow_tracker import Subject, Tracker, Emitter
 from snowplow_tracker import SelfDescribingJson
 import csv
+import time
 
 e = Emitter("10.0.52.22", port=8080)
 t = Tracker(e)
@@ -25,5 +26,6 @@ while 1 == 1 :
                 {"event": event_name,
                 "package": package,
                 "unit_id": unit_id,
-                "price": price
+                "price": price,
+                "timestamp": str(time.strftime("%Y-%m-%d %H:%m:%S"))
                 }))
