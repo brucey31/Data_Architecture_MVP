2175e8ae38b387aa772b32224b77623188e604a8
Bruce Pannaman <brucepannaman@Busuus-MacBook-Pro-9.local>
2016-09-27 19:41:06 +0100

    eod commit

--- Process_Kinesis_Kissmetrics.py
+++ Process_Kinesis_Kissmetrics.py
@@ -1,44 +1,42 @@
 from __future__ import print_function
 from botocore.vendored import requests
-import csv
-import json
 import base64
-import re
 
 print('Loading function')
 key = '7ce17c273559653b041e8df9e7f65716453ceffd'
 
 
 def lambda_handler(event, context):
-    iterator = 1
-    list = []
-    while iterator < 130:
-        list.append(iterator)
-        iterator = iterator + 1
-
     for record in event['Records']:
+
         # Kinesis data is base64 encoded so decode here
         payload = base64.b64decode(record['kinesis']['data'])
         print(payload)
+
+        # Change from tab delimited to dict
         paramstring = payload.split("\t")
-        print(paramstring)
 
-        if paramstring[4] == "struct":
-            uid = paramstring[9]
-            event = paramstring[20]
-            ts = paramstring[1]
-            unit_id = paramstring[22]
-            package = paramstring[21]
-            price = paramstring[23]
-            platform = paramstring[0]
+        # Grab the fields I want
+        if paramstring[5] == "struct":
+            event = paramstring[54]
+            ts = paramstring[2]
+            unit_id = paramstring[56]
+            package = paramstring[55]
+            price = paramstring[57]
+            platform = paramstring[1]
+            uid = paramstring[12]
 
-        request = requests.get(
-            'https://trk.kissmetrics.com/e?_n=%s&_k=%s&_p=%s&_t=%s&price=%s&platform=%s&unit_id=%s&package=%s' % (
-            event, key, uid, ts, price, platform, unit_id, package))
-        if request.status_code == 200:
-            success = "Sent %s, %s, %s, %s, %s, %s, %s" % (uid, event, ts, package, price, platform, unit_id)
-            print(success)
+            # Ping to KISSMETRICS
+            request = requests.get(
+                'https://trk.kissmetrics.com/e?_n=%s&_k=%s&_p=%s&_t=%s&price=%s&platform=%s&unit_id=%s&package=%s' % (
+                event, key, uid, ts, price, platform, unit_id, package))
+            if request.status_code == 200:
+                success = "Sent %s, %s, %s, %s, %s, %s, %s" % (uid, event, ts, package, price, platform, unit_id)
+                print(success)
+            else:
+                print('Something went wrong')
+
+            return success
+
         else:
-            print('Something went wrong')
-
-    return success
+            continue
--- Test_Snowplow_Connector.py
+++ Test_Snowplow_Connector.py
@@ -19,6 +19,7 @@ while bruce == "awesome":
         spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
         for row in spamreader:
             event_name = row[0]
+            print event_name
             unit_id = row[1]
             package = row[2]
             price = row[3]
@@ -31,7 +32,6 @@ while bruce == "awesome":
             t.subject.set_platform(platform).set_user_id(uid).set_lang("enc").set_ip_address(ip)
 
             # Send it to emitter
-            # t.track_struct_event("test", event_name, package, unit_id, float(price), None, calendar.timegm(time.gmtime()))
             t.track_struct_event(event_name, package, unit_id, price, None, None, timestamp)
 
             t.track_page_view("https://www.busuu.com/en/learn/start-learning/", "Enc Landing page", "https://www.busuu.com/en/learn/start-learning/?b_source=adwords&b_campaign=uk_en_web_brand_e&b_group=uk_en_web_brand_e&b_subnetwork=sn&b_term=busuu&b_placement=&utm_nooverride=1&gclid=CL-D_ajzr88CFU6eGwodq_YHzw")
