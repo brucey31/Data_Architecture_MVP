ceb3092a72086586df199816da473f2e9e68a540
Bruce Pannaman <brucepannaman@Busuus-MacBook-Pro-9.local>
2016-09-28 10:14:58 +0100

    kissmetrics now works

--- Process_Kinesis_Kissmetrics.py
+++ Process_Kinesis_Kissmetrics.py
@@ -1,3 +1,4 @@
+# testing string for base64 encoded string = QnVzdXUJcGMJMjAxNi0wOS0yODA4OjE3OjU0LjI0MwkyMDE2LTA5LTI4MDg6MTY6NTUuNzE1CTIwMTYtMDktMjgwODoxNjo1NS42ODcJc3RydWN0CTEwNjVjZTFiLTM2MzQtNDczMS1iOGRlLWY0MDk4OTUyNzE2MQlweS0wLjcuMglzc2MtMC42LjAta2luZXNpcwlraW5lc2lzLTAuOC4xLWNvbW1vbi0wLjIzLjEJMTIzNDU2ODExCTE5NS4xMTAuNjkuODYJOTVkNmQzMWMtYzA0ZC00ZWQxLThiMjktMTczMGYJR0IJSDkJTG9uZG9uCUVDNE4JNTEuNTE0MjA2CS0wLjA5MzA5Mzg3CUxvbmRvbglQdXJjaGFzZWQJNk1vbnRocwkxXzFfMV8xX0cJNjkuOTkJcHl0aG9uLXJlcXVlc3RzLzIuMTEuMQllbmMJRXVyb3BlL0xvbmRvbgkyMDE2LTA5LTI4MDg6MTY6NTUuNzE1CWNvbS5nb29nbGUuYW5hbHl0aWNzCWV2ZW50CWpzb25zY2hlbWEJMS0wLTBIOQlMb25kb24JRUM0Tgk1MS41MTQyMDYJLTAuMDkzMDkzODcJTG9uZG9uCVB1cmNoYXNlZAk2TW9udGhzCTFfMV8xXzFfRwk2OS45OQlweXRob24tcmVxdWVzdHMvMi4xMS4xCWVuYwlFdXJvcGUvTG9uZG9uCTIwMTYtMDktMjgwODoxNjo1NS43MTUJY29tLmdvb2dsZS5hbmFseXRpY3MJZXZlbnQJanNvbnNjaGVtYQkxLTAtMEg5CUxvbmRvbglFQzROCTUxLjUxNDIwNgktMC4wOTMwOTM4NwlMb25kb24JUHVyY2hhc2VkCTZNb250aHMJMV8xXzFfMV9HCTY5Ljk5CXB5dGhvbi1yZXF1ZXN0cy8yLjExLjEJZW5jCUV1cm9wZS9Mb25kb24JMjAxNi0wOS0yODA4OjE2OjU1LjcxNQljb20uZ29vZ2xlLmFuYWx5dGljcwlldmVudAlqc29uc2NoZW1hCTEtMC0wSDkJTG9uZG9uCUVDNE4JNTEuNTE0MjA2CS0wLjA5MzA5Mzg3CUxvbmRvbglQdXJjaGFzZWQJNk1vbnRocwkxXzFfMV8xX0cJNjkuOTkJcHl0aG9uLXJlcXVlc3RzLzIuMTEuMQllbmMJRXVyb3BlL0xvbmRvbgkyMDE2LTA5LTI4MDg6MTY6NTUuNzE1CWNvbS5nb29nbGUuYW5hbHl0aWNzCWV2ZW50CWpzb25zY2hlbWEJMS0wLTBIOQlMb25kb24JRUM0Tgk1MS41MTQyMDYJLTAuMDkzMDkzODcJTG9uZG9uCVB1cmNoYXNlZAk2TW9udGhzCTFfMV8xXzFfRwk2OS45OQlweXRob24tcmVxdWVzdHMvMi4xMS4xCWVuYwlFdXJvcGUvTG9uZG9uCTIwMTYtMDktMjgwODoxNjo1NS43MTUJY29tLmdvb2dsZS5hbmFseXRpY3MJZXZlbnQJanNvbnNjaGVtYQkxLTAtMEg5CUxvbmRvbglFQzROCTUxLjUxNDIwNgktMC4wOTMwOTM4NwlMb25kb24JUHVyY2hhc2VkCTZNb250aHMJMV8xXzFfMV9HCTY5Ljk5CXB5dGhvbi1yZXF1ZXN0cy8yLjExLjEJZW5jCUV1cm9wZS9Mb25kb24JMjAxNi0wOS0yODA4OjE2OjU1LjcxNQljb20uZ29vZ2xlLmFuYWx5dGljcwlldmVudAlqc29uc2NoZW1hCTEtMC0wSDkJTG9uZG9uCUVDNE4JNTEuNTE0MjA2CS0wLjA5MzA5Mzg3CUxvbmRvbglQdXJjaGFzZWQJNk1vbnRocwkxXzFfMV8xX0cJNjkuOTkJcHl0aG9uLXJlcXVlc3RzLzIuMTEuMQllbmMJRXVyb3BlL0xvbmRvbgkyMDE2LTA5LTI4MDg6MTY6NTUuNzE1CWNvbS5nb29nbGUuYW5hbHl0aWNzCWV2ZW50CWpzb25zY2hlbWEJMS0wLTBIOQlMb25kb24JRUM0Tgk1MS41MTQyMDYJLTAuMDkzMDkzODcJTG9uZG9uCVB1cmNoYXNlZAk2TW9udGhzCTFfMV8xXzFfRwk2OS45OQlweXRob24tcmVxdWVzdHMvMi4xMS4xCWVuYwlFdXJvcGUvTG9uZG9uCTIwMTYtMDktMjgwODoxNjo1NS43MTUJY29tLmdvb2dsZS5hbmFseXRpY3MJZXZlbnQJanNvbnNjaGVtYQkxLTAtMA==
 from __future__ import print_function
 from botocore.vendored import requests
 import base64
@@ -10,19 +11,19 @@ def lambda_handler(event, context):
     for record in event['Records']:
 
         # Kinesis data is base64 encoded so decode here
+        print(record['kinesis']['data'])
         payload = base64.b64decode(record['kinesis']['data'])
-        print(payload)
 
         # Change from tab delimited to dict
         paramstring = payload.split("\t")
 
         # Grab the fields I want
         if paramstring[5] == "struct":
-            event = paramstring[54]
+            event = paramstring[53]
             ts = paramstring[2]
-            unit_id = paramstring[56]
-            package = paramstring[55]
-            price = paramstring[57]
+            unit_id = paramstring[55]
+            package = paramstring[54]
+            price = paramstring[56]
             platform = paramstring[1]
             uid = paramstring[12]
 
