dc3beea438ef58e6a35ee25d83ae923fc1be6a08
Bruce Pannaman <brucepannaman@Busuus-MacBook-Pro-9.local>
2016-10-05 11:58:07 +0100

    last appboy file

---
+++ Process_Kinesis_Appboy.py
@@ -0,0 +1,84 @@
+# testing string for base64 encoded string = QnVzdXUJcGMJMjAxNi0wOS0yODA4OjE3OjU0LjI0MwkyMDE2LTA5LTI4MDg6MTY6NTUuNzE1CTIwMTYtMDktMjgwODoxNjo1NS42ODcJc3RydWN0CTEwNjVjZTFiLTM2MzQtNDczMS1iOGRlLWY0MDk4OTUyNzE2MQlweS0wLjcuMglzc2MtMC42LjAta2luZXNpcwlraW5lc2lzLTAuOC4xLWNvbW1vbi0wLjIzLjEJMTIzNDU2ODExCTE5NS4xMTAuNjkuODYJOTVkNmQzMWMtYzA0ZC00ZWQxLThiMjktMTczMGYJR0IJSDkJTG9uZG9uCUVDNE4JNTEuNTE0MjA2CS0wLjA5MzA5Mzg3CUxvbmRvbglQdXJjaGFzZWQJNk1vbnRocwkxXzFfMV8xX0cJNjkuOTkJcHl0aG9uLXJlcXVlc3RzLzIuMTEuMQllbmMJRXVyb3BlL0xvbmRvbgkyMDE2LTA5LTI4MDg6MTY6NTUuNzE1CWNvbS5nb29nbGUuYW5hbHl0aWNzCWV2ZW50CWpzb25zY2hlbWEJMS0wLTBIOQlMb25kb24JRUM0Tgk1MS41MTQyMDYJLTAuMDkzMDkzODcJTG9uZG9uCVB1cmNoYXNlZAk2TW9udGhzCTFfMV8xXzFfRwk2OS45OQlweXRob24tcmVxdWVzdHMvMi4xMS4xCWVuYwlFdXJvcGUvTG9uZG9uCTIwMTYtMDktMjgwODoxNjo1NS43MTUJY29tLmdvb2dsZS5hbmFseXRpY3MJZXZlbnQJanNvbnNjaGVtYQkxLTAtMEg5CUxvbmRvbglFQzROCTUxLjUxNDIwNgktMC4wOTMwOTM4NwlMb25kb24JUHVyY2hhc2VkCTZNb250aHMJMV8xXzFfMV9HCTY5Ljk5CXB5dGhvbi1yZXF1ZXN0cy8yLjExLjEJZW5jCUV1cm9wZS9Mb25kb24JMjAxNi0wOS0yODA4OjE2OjU1LjcxNQljb20uZ29vZ2xlLmFuYWx5dGljcwlldmVudAlqc29uc2NoZW1hCTEtMC0wSDkJTG9uZG9uCUVDNE4JNTEuNTE0MjA2CS0wLjA5MzA5Mzg3CUxvbmRvbglQdXJjaGFzZWQJNk1vbnRocwkxXzFfMV8xX0cJNjkuOTkJcHl0aG9uLXJlcXVlc3RzLzIuMTEuMQllbmMJRXVyb3BlL0xvbmRvbgkyMDE2LTA5LTI4MDg6MTY6NTUuNzE1CWNvbS5nb29nbGUuYW5hbHl0aWNzCWV2ZW50CWpzb25zY2hlbWEJMS0wLTBIOQlMb25kb24JRUM0Tgk1MS41MTQyMDYJLTAuMDkzMDkzODcJTG9uZG9uCVB1cmNoYXNlZAk2TW9udGhzCTFfMV8xXzFfRwk2OS45OQlweXRob24tcmVxdWVzdHMvMi4xMS4xCWVuYwlFdXJvcGUvTG9uZG9uCTIwMTYtMDktMjgwODoxNjo1NS43MTUJY29tLmdvb2dsZS5hbmFseXRpY3MJZXZlbnQJanNvbnNjaGVtYQkxLTAtMEg5CUxvbmRvbglFQzROCTUxLjUxNDIwNgktMC4wOTMwOTM4NwlMb25kb24JUHVyY2hhc2VkCTZNb250aHMJMV8xXzFfMV9HCTY5Ljk5CXB5dGhvbi1yZXF1ZXN0cy8yLjExLjEJZW5jCUV1cm9wZS9Mb25kb24JMjAxNi0wOS0yODA4OjE2OjU1LjcxNQljb20uZ29vZ2xlLmFuYWx5dGljcwlldmVudAlqc29uc2NoZW1hCTEtMC0wSDkJTG9uZG9uCUVDNE4JNTEuNTE0MjA2CS0wLjA5MzA5Mzg3CUxvbmRvbglQdXJjaGFzZWQJNk1vbnRocwkxXzFfMV8xX0cJNjkuOTkJcHl0aG9uLXJlcXVlc3RzLzIuMTEuMQllbmMJRXVyb3BlL0xvbmRvbgkyMDE2LTA5LTI4MDg6MTY6NTUuNzE1CWNvbS5nb29nbGUuYW5hbHl0aWNzCWV2ZW50CWpzb25zY2hlbWEJMS0wLTBIOQlMb25kb24JRUM0Tgk1MS41MTQyMDYJLTAuMDkzMDkzODcJTG9uZG9uCVB1cmNoYXNlZAk2TW9udGhzCTFfMV8xXzFfRwk2OS45OQlweXRob24tcmVxdWVzdHMvMi4xMS4xCWVuYwlFdXJvcGUvTG9uZG9uCTIwMTYtMDktMjgwODoxNjo1NS43MTUJY29tLmdvb2dsZS5hbmFseXRpY3MJZXZlbnQJanNvbnNjaGVtYQkxLTAtMA==
+from __future__ import print_function
+from botocore.vendored import requests
+import base64
+import json
+import ConfigParser
+from datetime import datetime
+
+config = ConfigParser.ConfigParser()
+ini = config.read('conf2.ini')
+
+APPBOY_APP_ID = config.get('APPBOY', 'APP_ID')
+APPBOY_APP_GROUP_ID = config.get('APPBOY', 'APP_GROUP_ID')
+
+print('Loading function')
+
+def lambda_handler(event, context):
+    for record in event['Records']:
+
+        # Kinesis data is base64 encoded so decode here
+        print(record['kinesis']['data'])
+        payload = base64.b64decode(record['kinesis']['data'])
+
+        # Change from tab delimited to dict
+        paramstring = payload.split("\t")
+        print(paramstring)
+
+        # Grab the fields I want
+        if paramstring[5] == "struct" and paramstring[53] != "page_view":
+            print("entered struct clause")
+            event = paramstring[53]
+            ts = datetime.now().strftime('%Y-%m-%d %H:%M:%S %Z%z')
+            language_learnt = paramstring[55]
+            interface_language = paramstring[54]
+            platform = paramstring[1]
+            uid = paramstring[12]
+            params = paramstring[56]
+            params = json.loads(params.replace("'", '"'))
+            # print(event)
+            # print(ts)
+            # print(language_learned)
+            # print(interface_language)
+            # print(platform)
+            # print(uid)
+            # print(params)
+
+            event_array = {}
+            event_array["external_id"] = uid
+            event_array["app_id"] = APPBOY_APP_ID
+            event_array["name"] = event
+            event_array["ts"] = ts
+            event_array["language_learnt"] = language_learnt
+            for param in params:
+                event_array[param] = params[param]
+
+            event_array_list = []
+            event_array_list.append(event_array)
+
+            print(event_array)
+
+            attributes = {}
+            attributes["external_id"] = uid
+            attributes["language"] = interface_language
+
+            attributes_list = []
+            attributes_list.append(attributes)
+
+            # Ping to AppBoy
+            # event_array = [{"external_id": "32767377", "app_id": APPBOY_APP_ID, "name": "Bruce_test", "time": ts}]
+            url = 'https://api.appboy.com/users/track'
+            headers = {"Content-Type": "application/json"}
+            data = {"app_group_id": APPBOY_APP_GROUP_ID, "attributes": attributes_list, "events": event_array_list}
+
+            data_to_app = json.dumps(data)
+
+            r = requests.post(url, data=data_to_app, headers=headers)
+
+            if r.status_code == 200:
+                print("Sent Successfully")
+            else:
+                print(r.content)
+
+        else:
+            continue
