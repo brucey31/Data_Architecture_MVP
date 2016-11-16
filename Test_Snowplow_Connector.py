889b540dfdfe0cfc625a162ea4b9993c6af150e1
Bruce Pannaman <brucepannaman@Busuus-MacBook-Pro-9.local>
2016-11-16 19:04:27 +0000

    better schema

--- Test_Snowplow_Connector.py
+++ Test_Snowplow_Connector.py
@@ -11,10 +11,9 @@ ip = socket.gethostbyname(socket.gethostname())
 
 bruce = "awesome"
 while bruce == "awesome":
-    with open('Kinesis_Test_Data.csv', 'rb') as csvfile:
+    with open('Kinesis_Test_Data2.csv', 'rb') as csvfile:
         spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
         for row in spamreader:
-
             event_name = row[0]
             timestamp = int((datetime.datetime.utcnow() - datetime.datetime(1970, 1, 1)).total_seconds())
 
@@ -55,54 +54,55 @@ while bruce == "awesome":
             #     items = [{"sku": "made_up_SKU", "price": price, "quantity": 1, "name": str(package)}]
             #     t.track_ecommerce_transaction("A-0000001234", price, email, None, None , None , None , None , currency, items , None , timestamp)
             #
-            if "purchased" or 'purchase_complete' or 'page_view' not in event_name:
-                interface_language = row[1]
-                language_learnt = row[2]
-                b_source = row[3]
-                b_campaign = row[8]
-                uid = row[5]
-                platform = row[4]
-                b_term = row[6]
-                b_group = row[7]
-                email = row[9]
 
-                custom_context = str(
-                    {"source": b_source, "campaign": b_campaign, "term": b_term, "group": b_group, "email": email})
+            interface_language = row[1]
+            language_learnt = row[2]
+            b_source = row[3]
+            b_campaign = row[8]
+            uid = row[5]
+            platform = row[4]
+            b_term = row[6]
+            b_group = row[7]
+            email = row[9]
 
-                # Set params for specific User
-                s = Subject()
-                t.subject.set_platform(platform).set_user_id(uid).set_lang("enc").set_ip_address(ip)
+            custom_context = str(
+                {"source": b_source, "campaign": b_campaign, "term": b_term, "group": b_group, "email": email})
 
-                # Send it to emitter
-                # t.track_struct_event(event_name, interface_language, language_learnt, custom_context, None, None, timestamp)
+            # Set params for specific User
+            s = Subject()
+            t.subject.set_platform(platform).set_user_id(uid).set_lang("enc").set_ip_address(ip)
+
+            # Send it to emitter
+            # t.track_struct_event(event_name, interface_language, language_learnt, custom_context, None, None, timestamp)
 
 
-                # t.track_unstruct_event({"schema": "iglu:com.busuu/standard_event/jsonschema/1-0-0",
-                #                         "data": {
-                #                             "event": event_name,
-                #                             "uid": uid,
-                #                             "language_learnt": language_learnt,
-                #                             "ts": timestamp,
-                #                             "interface_language": interface_language,
-                #                             "params": custom_context,
-                #                             "platform": platform,
-                #                             "app_id": "app_id_1",
-                #                             "version": "1.563_bruce",
-                #                             "environment": "brucetopia",
-                #                             "user_agent": "007_licence_to_kill"
-                #                         }})
+            # t.track_unstruct_event({"schema": "iglu:com.busuu/standard_event/jsonschema/1-0-0",
+            #                         "data": {
+            #                             "event": event_name,
+            #                             "uid": uid,
+            #                             "language_learnt": language_learnt,
+            #                             "ts": timestamp,
+            #                             "interface_language": interface_language,
+            #                             "params": custom_context,
+            #                             "platform": platform,
+            #                             "app_id": "app_id_1",
+            #                             "version": "1.563_bruce",
+            #                             "environment": "brucetopia",
+            #                             "user_agent": "007_licence_to_kill"
+            #                         }})
 
-                event = SelfDescribingJson(schema="iglu:com.busuu/standard_event/jsonschema/1-0-1",
-                                           data={
-                                                "event": event_name,
-                                                "uid": uid,
-                                                "language_learnt": language_learnt,
-                                                "interface_language": interface_language,
-                                                "params": custom_context,
-                                                "platform": platform,
-                                                "app_id": "app_id_1",
-                                                "version": "1.563_bruce",
-                                                "environment": "brucetopia",
-                                                "user_agent": "007_licence_to_kill"})
+            schema = "iglu:com.busuu/standard_event/jsonschema/1-0-0"
+            event = SelfDescribingJson(schema=schema,
+                                       data={
+                                           "event": event_name,
+                                           "uid": uid,
+                                           "language_learnt": language_learnt,
+                                           "interface_language": interface_language,
+                                           "params": custom_context,
+                                           "platform": platform,
+                                           "app_id": "app_id_1",
+                                           "version": "1.563_bruce",
+                                           "environment": "brucetopia",
+                                           "user_agent": "007_licence_to_kill"})
 
-                t.track_unstruct_event(event)
+            t.track_unstruct_event(event)
